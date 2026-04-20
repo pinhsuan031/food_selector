07257753dd3779964cdcd4d1d192b251f8e10295
pinhsuan031 <pinhsuan031@gmail.com>
2026-04-20 14:33:42 +0800

    Add select all, update leaflet map

--- static/map.js
+++ static/map.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([25.0336646, 121.5438858], 16);
+var map = L.map('map').setView([25.0333673, 121.5433068], 16.5);
 
-var marker = L.marker([25.0336646, 121.5438858]).addTo(map);
+var marker = L.marker([25.0333673, 121.5433068]).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
